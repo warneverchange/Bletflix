fb66f0ce56f5ea7a844a66cb0c0f39660b468a52
warneverchange <ceckinkonstantin@gmail.com>
2023-01-22 18:55:17 +0500

    init commit

---
+++ src/main/java/com/videoservice/bletflix/BletflixApplication.java
@@ -0,0 +1,13 @@
+package com.videoservice.bletflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BletflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BletflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/videoservice/bletflix/BletflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoservice.bletflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BletflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
