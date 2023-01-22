5c677c445d5f6d8deacaa2a7f57b493ac0066056
warneverchange <ceckinkonstantin@gmail.com>
2023-01-22 19:30:43 +0500

    add home controller & home page

---
+++ src/main/java/com/videoservice/bletflix/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.videoservice.bletflix.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping()
+    public String getHomePage() {
+        return "index.html";
+    }
+}
